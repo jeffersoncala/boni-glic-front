69cedb88ca9f9c563e27b7b430258b675b3f6680
Jefferson Calazans <almeidacalazans@yahoo.com.br>
2022-07-20 01:20:58 -0300

    [feat] config iniciais

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import Auth from '@websanova/vue-auth'
+import VueAuthHttp from '@websanova/vue-auth/drivers/http/axios.1.x'
+import VueAuthRouter from '@websanova/vue-auth/drivers/router/vue-router.2.x'
 
 import router from '@/router'
+
+Vue.use(VueAxios, axios)
+
+Vue.config.productionTip = false
+
+Vue.axios.defaults.baseURL = process.env.VUE_APP_ROOT_API
+Vue.axios.defaults.headers.common['Content-Type'] = 'application/x-www-form-urlencoded'
+
+
 
 //Bootstrap
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
@@ -16,7 +30,35 @@ Vue.config.productionTip = false
 
 Vue.router = router
 
+Vue.use(Auth, {
+	auth: {
+		request: function (req, token) {
+      console.info('request')
+			this.options.http._setHeaders.call(this, req, { 'boni-glic-token': token })
+		},
+		response: function (res) {
+      console.info('response')
+			const headers = res.data
+			return headers.token
+		}
+	},
+	fetchData: { url: '/usuarios/minha_conta', method: 'GET' },
+	http: VueAuthHttp,
+	router: VueAuthRouter,
+	tokenDefaultKey: 'boni-glic-token',
+	rolesVar: 'perfil',
+	authRedirect: { path: '/login' },
+	forbiddenRedirect: { path: '/login' },
+	parseUserData: function (data) {
+		return data
+	},
+	refreshData: {
+		enabled: false
+	}
+})
+
 new Vue({
+  el: '#app',
   router,
   render: h => h(App),
 }).$mount('#app')
